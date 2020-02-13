4954b568d55f68569f61a3d37aa3d4a1bf4ba447
Sergiy <48173627+SergiyMolchan@users.noreply.github.com>
2020-02-13 17:16:45 +0200

    live validation form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,12 +1,149 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Button from '@material-ui/core/Button';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import LockIcon from '@material-ui/icons/Lock';
+import HowToRegIcon from '@material-ui/icons/HowToReg';
+import {validFormCreator, validFieldCreator,isRequiredFieldCreator, minLengthCreator, maxLengthCreator, repeatPasswordCreator} from '../tools/validators';
 import {registration} from '../actions/registration';
 
+const useStyles = makeStyles(
+  theme => ({
+    root: {
+      minWidth: 275,
+      maxWidth: 760,
+      width: '100%',
+      textAlign: 'center',
+      margin: '0px 10px'
+    },
+    wrapper: {
+      width: '100%',
+      padding: '50px 0px',
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  }));
+
 function Registration() {
+  const classes = useStyles();
+  const [login = '', setLogin] = useState();
+  const [password  = '', setPassword] = useState();
+  const [repeatPassword  = '', setRepearPassword] = useState();
+  const [isTouchedLogin = false, TouchLogin] = useState();
+  const [isTouchedPassword, TouchPassword] = useState();
+  const [isTouchedRepeatPassword, TouchRepeatPassword] = useState();
+
+  const isReqiredLogin = isRequiredFieldCreator(login);
+  const isReqiredPassword = isRequiredFieldCreator(password);
+  const isReqiredRepeatPassword = isRequiredFieldCreator(repeatPassword);
+  const minLength5 = minLengthCreator(5);
+  const maxLenght30 = maxLengthCreator(30);
+  const repeatPasswordValidator = repeatPasswordCreator(password, repeatPassword);
+  const validFieldLogin = validFieldCreator([isReqiredLogin(), minLength5(login), maxLenght30(login)]);
+  const validFieldPassword = validFieldCreator([isReqiredPassword(), minLength5(password), maxLenght30(password)]);
+  const validFieldRepeatPassword = validFieldCreator([isReqiredRepeatPassword(), repeatPasswordValidator()]);
+  const isValidRegistrationForm = validFormCreator([validFieldLogin(true), validFieldPassword(true), validFieldRepeatPassword(true)]);
   return (
-      <>
-        <p>register</p>
-      </>
+    <div className={classes.wrapper}>
+      <Card className={classes.root}>
+        <CardHeader
+            title="Registration"
+            subheader="Create new account"
+        />
+        <CardActions>
+          <form className={classes.root} noValidate autoComplete="off">
+            <TextField
+              onChange={e => setLogin(e.target.value)}
+              onBlur={() => TouchLogin(true)}
+              value={login}
+              helperText={isTouchedLogin === true ? validFieldLogin() : false}
+              error={isTouchedLogin === true ? validFieldLogin(true) : false}
+              required
+              fullWidth
+              className={classes.margin}
+              label="Login"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+            <TextField
+              onChange={e => setPassword(e.target.value)}
+              onBlur={() => TouchPassword(true)}
+              value={password}
+              helperText={isTouchedPassword === true ? validFieldPassword() : false}
+              error={isTouchedPassword === true ? validFieldPassword(true) : false}
+              fullWidth
+              required 
+              className={classes.margin}
+              label="Password" 
+              type="password" 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+            <TextField
+              onChange={e => setRepearPassword(e.target.value)}
+              onBlur={() => TouchRepeatPassword(true)}
+              value={repeatPassword}
+              helperText={isTouchedRepeatPassword === true ? validFieldRepeatPassword() : false}
+              error={isTouchedRepeatPassword === true ? validFieldRepeatPassword(true) : false}
+              fullWidth
+              required 
+              className={classes.margin}
+              label="Repeat password" 
+              type="password" 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              endIcon={<HowToRegIcon/>}
+              disabled={isValidRegistrationForm()}
+            >
+              Create new account
+            </Button>
+          </form>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
 
---
+++ src/tools/validators.js
@@ -0,0 +1,35 @@
+export const validFormCreator = (fields = []) => () => {
+    return -1 !== fields.indexOf(true);
+}
+
+// Function creator return is function for checking the form field for validity. Accepts an array of validators.
+// the function created by the creator of the function takes a boolean value as a parameter
+// returns an array of errors by default; if passed true returns a boolean value
+export const validFieldCreator = (validators = []) => (isBool = false) => {
+    if(isBool){
+        return -1 !== validators.map(item => !!item).indexOf(true);
+    } else {
+        return validators;
+    }
+}
+
+// validators creators func
+export const isRequiredFieldCreator = value => () => {
+    if(value.length === 0) return `Field is required. `;
+    return undefined;
+}
+
+export const minLengthCreator = minLength => value => {
+    if(value.length < minLength) return `Min length is ${minLength} symbols. `;
+    return undefined;
+}
+
+export const maxLengthCreator = maxLength => value => {
+    if(value.length > maxLength) return `Max length is ${maxLength} symbols. `;
+    return undefined;
+}
+
+export const repeatPasswordCreator = (password, repeatPassword) => () =>{
+    if(password !== repeatPassword) return `Passwords do not match. `;
+    return undefined;
+}
